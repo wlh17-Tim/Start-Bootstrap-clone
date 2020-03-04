b455223277b8c198f84f09396be780e97abc6e3a
Timothy Barna <audioclimb@gmail.com>
2020-03-04 15:27:07 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="main-header">
+        <h1 className="logo">Start Bootstrap</h1>
+        <>
+              <nav className='header-links'>
+                <p>SERVICES</p>
+                <p>PORTFOLIO</p>
+                <p>ABOUT</p>
+                <p>TEAM</p>
+                <p>CONTACT</p>
+              </nav>
+            </>
       </header>
     </div>
   );
+}
 }
 
 export default App;
