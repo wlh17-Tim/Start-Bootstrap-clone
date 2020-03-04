b0a59f132b685b1c898c354be5a4e2ee263f671e
Timothy Barna <audioclimb@gmail.com>
2020-03-04 16:58:03 -0700

    still working

--- src/App.js
+++ src/App.js
@@ -2,13 +2,30 @@ import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      toggleDropdown: false
+    }
+  }
+
+  handleToggle = () => {
+    this.setState({toggleDropdown: !this.state.toggleDropdown})
+  }
+
   render(){
   return (
     <div className="App">
       <header className="main-header">
         <h1 className="logo">Start Bootstrap</h1>
+          {this.state.toggleDropdown
+          ? (
         <>
-              <nav className='header-links'>
+              <span className='dropdown-toggle' onClick={this.handleToggle}>
+                <p>MENU &#9776;</p>
+                
+                </span>
+              <nav className='mobile-links'>
                 <p>SERVICES</p>
                 <p>PORTFOLIO</p>
                 <p>ABOUT</p>
@@ -16,7 +33,29 @@ class App extends Component {
                 <p>CONTACT</p>
               </nav>
             </>
+          )
+          : (
+            <span className='dropdown-toggle' onClick={this.handleToggle}>
+              <p>MENU &#9776;</p>
+              
+              </span>
+          )
+          }
+          <nav className='header-links'>
+                <p>SERVICES</p>
+                <p>PORTFOLIO</p>
+                <p>ABOUT</p>
+                <p>TEAM</p>
+                <p>CONTACT</p>
+              </nav>
       </header>
+      <div className=''>
+        <p>Welcome To Our Studio</p>
+        <p>IT'S NICE TO MEET YOU</p>
+        <div>
+          <p>TELL ME MORE</p>
+        </div>
+      </div>
     </div>
   );
 }
